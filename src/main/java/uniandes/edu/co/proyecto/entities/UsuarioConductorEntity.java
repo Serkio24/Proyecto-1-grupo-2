615c8384033fc581e983fcb6ce043e8d84664d03
Laura Fernanda Martinez Galindo <lf.martinezg1@uniandes.edu.co>
2025-09-28 21:38:20 -0500

    cambios

--- src/main/java/uniandes/edu/co/proyecto/entities/UsuarioConductorEntity.java
+++ src/main/java/uniandes/edu/co/proyecto/entities/UsuarioConductorEntity.java
@@ -1,23 +1,17 @@
-package uniandes.edu.co.proyecto.modelo;
+package uniandes.edu.co.proyecto.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="usuarios_conductores")
-public class UsuarioConductorEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class UsuarioConductorEntity extends UsuarioEntity{
 
     private String estado;
 
     // Constructor con parámetros
-    public UsuarioConductorEntity(String estado) {
+    public UsuarioConductorEntity(String nombre, String correoElectronico, Long numeroCelular, Long numeroCedula, String estado) {
+        super(nombre, correoElectronico, numeroCelular, numeroCedula);
         this.estado = estado;
     }
 
@@ -25,19 +19,11 @@ public class UsuarioConductorEntity {
     public UsuarioConductorEntity() {}
 
     // Getters
-    public Long getId() {
-        return id;
-    }
-
     public String getEstado() {
         return estado;
     }
 
     // Setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setEstado(String estado) {
         this.estado = estado;
     }
