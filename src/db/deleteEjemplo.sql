ea1f9704212063596eb528a25727a94a827cccb9
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2025-09-29 20:08:26 -0500

    poblando

---
+++ src/db/deleteEjemplo.sql
@@ -0,0 +1,15 @@
+-- Eliminar datos de todas las tablas respetando dependencias
+DELETE FROM reviews;
+DELETE FROM viajes;
+DELETE FROM servicios_destinos;
+DELETE FROM servicios;
+DELETE FROM tarifas;
+DELETE FROM puntos_geograficos;
+DELETE FROM ciudades;
+DELETE FROM disponibilidades;
+DELETE FROM franjas_horarias;
+DELETE FROM tarjetas_credito;
+DELETE FROM conductor_vehiculos;
+DELETE FROM niveles_vehiculo;
+DELETE FROM vehiculos;
+DELETE FROM usuarios;
